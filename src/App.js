02cf8d0082441872f64f95e9b69eff57585741a4
James F. Thomas <james.fthomas@yahoo.com>
2020-12-27 11:07:27 -0600

    (add) materialze UI desgin package implmented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+// Import React framework and hooks
+import React, { useEffect } from 'react';
+// Import materialize package
+import 'materialize-css/dist/css/materialize.min.css';
+// Import JS from materialize package
+import M from 'materialize-css/dist/js/materialize.min.js'
+// Import local CSS file
 import './App.css';
 
 const App = () => {
+
+  // Hook will automatically initialize.invoke functions of component render
+  useEffect(()=>{
+    // Initialize Materialize JS in application
+    M.AutoInit();
+  })
   return (
     <div className="App">
       My App
